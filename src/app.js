8b0847076bc71376aaec348b2d099b950948ffb1
Sthefanie <sthefaniefernanda19@gmail.com>
2025-01-29 01:27:49 -0300

    att dependencias

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const routes = require('./routes');
-const app = express();
+import express from 'express';
+import routes from './routes';
 
 class App{
 
     constructor(){
-        this.server = express();+
+        this.server = express(); 
 
         this.middlewares();
         this.routes();
     }
 
     middlewares(){
-        this.server.use(express.json)
+        this.server.use(express.json());
     }
 
     routes(){
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const app = require('./app');
+import app from './app';
 
 app.listen(3333, '0.0.0.0', () => {
     console.log('Server running on http://0.0.0.0:3333');
