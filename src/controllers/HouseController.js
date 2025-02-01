fda94a8d6f6a8b7bde928d1eeff2a57e86e7a8b2
Sthefanie <sthefaniefernanda19@gmail.com>
2025-02-01 19:15:02 -0300

    ajustes login

---
+++ src/controllers/HouseController.js
@@ -0,0 +1,10 @@
+class HouseController{
+
+    async store(req, res){
+        console.log(req.body);
+        console.log(req.file);
+        return res.json({ok: true})
+    }
+}
+
+export default new HouseController();
