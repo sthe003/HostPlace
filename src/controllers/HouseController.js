9e4b7439f9ef8623259f5751709881c23a9b920a
Sthefanie <sthefaniefernanda19@gmail.com>
2025-02-21 01:44:59 -0300

    att

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -3,6 +3,14 @@ import House from '../models/House';
 
 class HouseController{
 
+    async index(req, res){
+        const {status} = req.query;
+
+        const houses = await House.find({status});
+
+        return res.json(houses);
+    }
+
     async store(req, res){
         const { filename } = req.file;
         const {description, price, location, status} = req.body;
@@ -19,6 +27,26 @@ class HouseController{
 
         return res.json(house)
     }
+
+    async update (req, res){
+        const {filename} = req.file;
+        const {house_id} = req.params;
+        const {description, price, location, status} = req.body;
+        const {user_ID} = req.headers;
+
+        const houses = await House.updateOne({ _id: house_id }, {
+            user: user_ID,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            status,
+        });
+
+        return res.json(houses);
+    }
+
+
 }
 
 export default new HouseController();
